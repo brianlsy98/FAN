36313ec58fabd70a3d5f0592615ef4cffcfa24bd
brianlsy <brianlsy98@gmail.com>
2026-03-30 23:31:00 -0500

    fan

--- offline-to-online/bash_scripts/cube-double/faql.sh
+++ offline-to-online/bash_scripts/cube-double/faql.sh
@@ -1,4 +1,4 @@
-for seed in 0 1 2; do
+for seed in 0 1 2 3 4 5 6 7; do
   for agent_name in faql; do
     for alpha1 in 100; do
       for alpha2 in 0; do
@@ -8,33 +8,7 @@ for seed in 0 1 2; do
             echo "--- cube-double-play-singletask-v0 with (a1,a2,o1,o2): ($alpha1,$alpha2,$online_alpha1,$online_alpha2) ---"
             python offline-to-online/offline_to_online.py \
               --seed=${seed} \
-              --env_name="cube-double-play-singletask-task1-v0" \
-              --agent=agents/${agent_name}.py \
-              --agent.alpha1=${alpha1} \
-              --agent.alpha2=${alpha2} \
-              --agent.discount=0.995 \
-              --agent.online_alpha1=${online_alpha1} \
-              --agent.online_alpha2=${online_alpha2} \
-              --online_steps=1000000
-            echo ""
-          done
-        done
-      done
-    done
-  done
-done
-
-for seed in 0 1 2; do
-  for agent_name in faql; do
-    for alpha1 in 100; do
-      for alpha2 in 3; do
-        for online_alpha1 in 100; do
-          for online_alpha2 in 0; do
-            echo ""
-            echo "--- puzzle-4x4-play-singletask-v0 with (a1,a2,o1,o2): ($alpha1,$alpha2,$online_alpha1,$online_alpha2) ---"
-            python offline-to-online/offline_to_online.py \
-              --seed=${seed} \
-              --env_name="puzzle-4x4-play-singletask-task1-v0" \
+              --env_name="cube-double-play-singletask-v0" \
               --agent=agents/${agent_name}.py \
               --agent.alpha1=${alpha1} \
               --agent.alpha2=${alpha2} \
